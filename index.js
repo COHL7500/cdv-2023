dc60de2e17841f24af199ae195a63ab1ac3837f7
COHL7500 <cohl7500@gmail.com>
2023-07-10 14:04:54 +0200

    feat!: init

---
+++ index.js
@@ -0,0 +1,74 @@
+//GOAL: can we set up a local coding environment, webpage and draw 1 shape on it?
+
+
+var circles = d3.selectAll("circle").attr("r",100);
+
+
+
+
+// var w = 500;
+// var h = 500;
+// var rad = 20;
+
+// var svg = d3.select("svg")
+// 			.attr("width",w)
+// 			.attr("height",h);
+
+// var circles = d3.selectAll("circle")
+// 				.attr("r", rad)
+// 				.attr("cx", w/2)
+// 				.attr("cy", h/2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var w = 500;
+// var h = 500;
+// var rad = 20;
+
+// var svg = d3.select("svg")
+// 			.attr("width",w)
+// 			.attr("height",h);
+
+// var circles = d3.selectAll("circle")
+// 				.attr("r", rad)
+// 				.attr("cx", w/2)
+// 				.attr("cy", h/2);
