66325099d0c6634d5fff393d67ce94f892c6e1d4
COHL7500 <cohl7500@gmail.com>
2023-07-12 11:29:49 +0200

    add changes

--- index.ts
+++ index.ts
@@ -1,20 +1,49 @@
-//GOAL: can we set up a local coding environment, webpage and draw 1 shape on it?
 
-const width = 400;
-const height = 400;
-const rad = 20;
+
+/*
+
+// DAY 2
+
+const width: number = 500;
+const height: number = 500;
+const leftMargin: number = 20;
+const canvas = d3.select("#canvas")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+const cupsCoffee: number[] = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0];
+
+// d = data, i = index
+
+const coffeeCircles = canvas.selectAll("circle")
+    .data(cupsCoffee)
+    .join("circle")
+    .attr("cx",  (d, i) =>  leftMargin + 10 + (i * 20))
+    .attr("cy", height / 2)
+    .attr("r", d => (d == 0 ? 1 : d * 10))
+    .attr("fill", d => (d == 0 ? "pink" : "purple"))
+    .attr("stroke", d => (d == 0 ? "pink" : "purple"));
+
+ */
+
+/*
+// DAY 1
+
+const width: number = 400;
+const height: number = 400;
+const rad: number = 30;
 
 let canvas = d3.select("svg")
     .attr("width", width)
     .attr("height", height)
-
-
+    .style("background", "purple");
 
 const circle1 = canvas.append("circle")
                         .attr("cx", 60)
                         .attr("cy", 60)
                         .attr("r", rad + 15)
-                        .attr("fill", "#A0A0A0")
+                        .attr("fill", "#A0A0A0") // this is grey;
 
 
 const circle2 = canvas.append("circle")
@@ -36,8 +65,7 @@ const circle4 = canvas.append("circle")
     .attr("cy", 50)
     .attr("r", rad)
     .attr("fill", "orange");
-
-
+*/
 
 // var w = 500;
 // var h = 500;
