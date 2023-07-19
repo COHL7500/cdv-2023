3bee0cbc76ec12040b053d4be886b46c1a8c8c69
COHL7500 <cohl7500@gmail.com>
2023-07-19 13:01:03 +0200

    feat: add scss

--- index.ts
+++ index.ts
@@ -3,33 +3,34 @@ type birthdayEntry = {
     name: string,
     age: number,
     month: number,
-    day: number
-}
+    day: number,
+};
 
-const entryAmount = 20
+const entryAmount = 20;
 
 const birthdayData: birthdayEntry[] = Array(entryAmount);
 
 // set the dimensions and margins of the graph
-const margin = {top: 10, right: 10, bottom: 50, left: 60},
+const margin = {top: 30, right: 70, bottom: 80, left: 60},
     width = 650 - margin.left - margin.right,
     height = 650 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 const svg: d3.Selection<SVGElement, any, HTMLElement, any> = d3.select("#canvas")
     .append("svg")
+    .attr("class", "svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+        "translate(" + margin.left + "," + margin.top + ")")
 
 const months: number[] = d3.range(1, 13);
 const days: number[] = d3.range(1, 32);
 
 // Build X scales and axis:
-const x = d3.scaleBand()
-    .range([ 0, width])
+const xScale = d3.scaleBand()
+    .range([0, width])
     .domain(months.map(String))
 
 /*
@@ -38,7 +39,8 @@ svg.append("g")
     .call(d3.axisBottom(x));
 
  */
-const y = d3.scaleBand()
+
+const yScale = d3.scaleBand()
     .range([height, 0])
     .domain(days.map(String))
 
@@ -86,29 +88,55 @@ function randomDay(): number {
 svg.append("text")
     .attr("text-anchor", "end")
     .attr("transform", "rotate(-90)")
-    .attr("y", -margin.left + 20)
+    .attr("y", -margin.left + 40)
     .attr("x", -height / 2)
+    //style("font-family", "Arial")
+    .attr("class", "graph-text")
     .text("Day")
 
 svg.append("text")
     .attr("text-anchor", "end")
-    .attr("y", height + margin.top + 35)
+    .attr("transform", "rotate(-45)")
+    .attr("y", -margin.left + 40)
+    .attr("x", -height + 555)
+    //.style("font-family", "Arial")
+    //.style("font-style", "italic")
+    .attr("class", "explain-text")
+    .attr("opacity", 0.15)
+    .text("31st")
+
+svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("y", height + margin.top + 5)
     .attr("x", width / 2)
+    .attr("class", "graph-text")
     .text("Month")
+
+svg.append("text")
+    .attr("text-anchor", "end")
+    //.attr("transform", "rotate(15)")
+    .attr("y", height + margin.top + 5)
+    .attr("x", width + 40)
+    //.style("font-family", "Arial")
+    //.style("font-style", "italic")
+    .attr("class", "explain-text")
+    .attr("opacity", 0.15)
+    .text("December")
 
 // Creation of the tooltip
 
 const tooltip: d3.Selection<HTMLDivElement, any, HTMLElement, any> = d3.select("#canvas")
     .append("div")
-    .style("opacity", 0)
+    //.style("opacity", 0)
+
     .attr("class", "tooltip")
-    .style("background-color", "white")
-    .style("border", "solid")
-    .style("border-width", "2px")
-    .style("border-radius", "5px")
-    .style("padding", "5px")
-    .style("position", "absolute")
-    .style("max-width", "200px")
+    //.style("background-color", "white")
+    //.style("border", "solid")
+    //.style("border-width", "2px")
+    //.style("border-radius", "5px")
+    //.style("padding", "5px")
+    //.style("position", "absolute")
+    //.style("max-width", "200px")
 
 const onMouseOver = function() {
     tooltip.style("opacity", 1)
@@ -122,39 +150,42 @@ const onMouseMove = function (event: any, d: birthdayEntry) {
 }
 
 const onMouseLeave = function() {
-    tooltip.style("opacity", 0)
-}
+    tooltip.style("opacity", 0);
+};
 
 
 // Make a grey grid for the heatmap
-const gridGroup = svg.append("g")
-months.forEach(function(m) {
-    days.forEach(function(d) {
+const gridGroup = svg.append("g");
+
+months.forEach(m =>
+    days.forEach(d =>
         gridGroup.append("rect")
-            .attr("x", x(String(m)))
-            .attr("y", y(String(d)))
-            .attr("width", x.bandwidth())
-            .attr("height", y.bandwidth())
+            .attr("x", xScale(String(m)))
+            .attr("y", yScale(String(d)))
+            .attr("width", xScale.bandwidth())
+            .attr("height", yScale.bandwidth())
             .style("stroke", "#ececec")
-            .style("fill", "none");
-    });
-});
+            .style("fill", "none")
+    )
+);
+
+
 
 // from age of 0 to 100, go from green to red.
 
 const ageRangeColor = d3.scaleLinear<string, number>()
-    .range(["#2bcc2b", "#FF0000"])
-    .domain([0, 100])
+    .range(["#ced3e5", "#164d72"])
+    .domain([0, 100]);
 
 // Creation of birthday rectangles
 svg.selectAll()
     .data(birthdayData)
     .enter()
     .append("rect")
-    .attr("x", d => x(String(d.month)) )
-    .attr("y", d => y(String(d.day)) )
-    .attr("width", x.bandwidth())
-    .attr("height", y.bandwidth())
+    .attr("x", d => xScale(String(d.month)) )
+    .attr("y", d => yScale(String(d.day)) )
+    .attr("width", xScale.bandwidth())
+    .attr("height", yScale.bandwidth())
     .style("fill", d => ageRangeColor(d.age))
     .on("mouseover", onMouseOver)
     .on("mousemove", onMouseMove)
