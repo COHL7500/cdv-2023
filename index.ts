5a46142cf5df6a44b0443e21d0113f36ad56c348
COHL7500 <cohl7500@gmail.com>
2023-07-20 14:09:56 +0200

    feat: finish deliverable for day 7

--- index.ts
+++ index.ts
@@ -1,192 +1,146 @@
-// type (could be interface as well) for birthday entries
-type birthdayEntry = {
-    name: string,
-    age: number,
-    month: number,
-    day: number,
+const width = 1000;
+const height = 800;
+const margin = {top: 100, right: 100, left: 100, bottom: 100};
+
+type dataInterval = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+
+type SkyEntry = {
+    minute: dataInterval
+    cloudCov: dataInterval
+    size: dataInterval
 };
 
-const entryAmount = 20;
+const entryAmount = 50;
 
-const birthdayData: birthdayEntry[] = Array(entryAmount);
+let skyDataMap: Map<number, SkyEntry[]> = new Map<dataInterval, SkyEntry[]>();
 
-// set the dimensions and margins of the graph
-const margin = {top: 30, right: 70, bottom: 80, left: 60},
-    width = 650 - margin.left - margin.right,
-    height = 650 - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-const svg: d3.Selection<SVGElement, any, HTMLElement, any> = d3.select("#canvas")
-    .append("svg")
-    .attr("class", "svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")")
-
-const months: number[] = d3.range(1, 13);
-const days: number[] = d3.range(1, 32);
-
-// Build X scales and axis:
-const xScale = d3.scaleBand()
-    .range([0, width])
-    .domain(months.map(String))
-
-/*
-svg.append("g")
-    .attr("transform", "translate(0," + (height + 2) + ")")
-    .call(d3.axisBottom(x));
-
- */
-
-const yScale = d3.scaleBand()
-    .range([height, 0])
-    .domain(days.map(String))
-
-/*
-svg.append("g")
-    .call(d3.axisLeft(y));
- */
+for (let i = 0; i < 10; i++) {
+    skyDataMap.set(i + 1, [])
+}
 
 for (let i = 0; i < entryAmount; i++) {
-        birthdayData[i] = randomBirthdayEntry()
+    const entry = generateSkyEntry();
+    skyDataMap.get(entry.minute)!.push(entry);
+}
+
+function generateSkyEntry(min?: number): SkyEntry {
+
+    const maxInterval = 10;
+    const minInterval = 1;
+
+    function randomSkyData(): dataInterval {
+        return (Math.floor(Math.random() * (maxInterval - minInterval + 1) + minInterval)) as dataInterval;
     }
 
-    /*
-    [
-    {name: "Roger", age: 19, month: 7, day: 13},
-    {name: "Michale", age: 33, month: 5, day: 1},
-    {name: "Bastjan", age: 100, month: 9, day: 21},
-    {name: "Baby Man", age: 0, month: 3, day: 2},
-    {name: "Middle Age Man", age: 50, month: 2, day: 2},
-    {name: "Richard Richardson", age: 69, month: 12, day: 24},
-    {name: "Laetitia Sadier", age: 22, month: 4, day: 1}
-]
-     */
-
-function randomBirthdayEntry(): birthdayEntry {
-    return {name: "Test", age: randomAge(), month: randomMonth(), day: randomDay()}
+    return {
+        minute: min ? min as dataInterval : randomSkyData(),
+        cloudCov: randomSkyData(),
+        size: randomSkyData(),
+    }
 }
 
-function randomRange(min, max): number { // min and max included
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
+const svg = d3.select("#canvas")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
 
-function randomAge(): number {
-    return randomRange(0, 100);
-}
+const triangleSize = 38;
 
-function randomMonth(): number {
-    return randomRange(1, 12);
-}
+const xScale = d3.scaleLinear<number>()
+    .domain([0, 20])
+    .range([margin.left, width-(margin.right)]);
 
-function randomDay(): number {
-    return randomRange(1, 31);
-}
+const yScale = d3.scaleBand<number>()
+    .padding(.1)
+    .domain(Array.from(skyDataMap.keys()))
+    .range( [height-margin.top, margin.bottom]);
 
-svg.append("text")
-    .attr("text-anchor", "end")
-    .attr("transform", "rotate(-90)")
-    .attr("y", -margin.left + 40)
-    .attr("x", -height / 2)
-    //style("font-family", "Arial")
-    .attr("class", "graph-text")
-    .text("Day")
+const colorScale = d3.scaleLinear<string, number>()
+    .domain([1, 10])
+    .range(["#eeeeee", "#595959"])
 
-svg.append("text")
-    .attr("text-anchor", "end")
-    .attr("transform", "rotate(-45)")
-    .attr("y", -margin.left + 40)
-    .attr("x", -height + 555)
-    //.style("font-family", "Arial")
-    //.style("font-style", "italic")
-    .attr("class", "explain-text")
-    .attr("opacity", 0.15)
-    .text("31st")
-
-svg.append("text")
-    .attr("text-anchor", "end")
-    .attr("y", height + margin.top + 5)
-    .attr("x", width / 2)
-    .attr("class", "graph-text")
-    .text("Month")
-
-svg.append("text")
-    .attr("text-anchor", "end")
-    //.attr("transform", "rotate(15)")
-    .attr("y", height + margin.top + 5)
-    .attr("x", width + 40)
-    //.style("font-family", "Arial")
-    //.style("font-style", "italic")
-    .attr("class", "explain-text")
-    .attr("opacity", 0.15)
-    .text("December")
-
-// Creation of the tooltip
+const innerTriangleScale = d3.scaleLinear<number>()
+    .domain([1, 10])
+    .range([2.0, 1.0])
 
 const tooltip: d3.Selection<HTMLDivElement, any, HTMLElement, any> = d3.select("#canvas")
     .append("div")
-    //.style("opacity", 0)
-
+    .style("opacity", 0)
     .attr("class", "tooltip")
-    //.style("background-color", "white")
-    //.style("border", "solid")
-    //.style("border-width", "2px")
-    //.style("border-radius", "5px")
-    //.style("padding", "5px")
-    //.style("position", "absolute")
-    //.style("max-width", "200px")
 
 const onMouseOver = function() {
     tooltip.style("opacity", 1)
 }
 
-const onMouseMove = function (event: any, d: birthdayEntry) {
+const onMouseMove = function (event: any, d: SkyEntry) {
     tooltip
-        .html("Name: " + d.name + "<br>Age: " + d.age)
-        .style("left", (d3.pointer(event)[0] + 100) + "px")
-        .style("top", (d3.pointer(event)[1]) + "px")
+        .html("Minute: " + d.minute + "<br>Size: " + d.size + "<br>CloudCov: " + d.cloudCov)
+        .style("left", event.pageX + 10 + "px")
+        .style("top", event.pageY + 10 + "px")
 }
 
 const onMouseLeave = function() {
-    tooltip.style("opacity", 0);
-};
+    tooltip.style("opacity", 0)
+}
 
 
-// Make a grey grid for the heatmap
-const gridGroup = svg.append("g");
-
-months.forEach(m =>
-    days.forEach(d =>
-        gridGroup.append("rect")
-            .attr("x", xScale(String(m)))
-            .attr("y", yScale(String(d)))
-            .attr("width", xScale.bandwidth())
-            .attr("height", yScale.bandwidth())
-            .style("stroke", "#ececec")
-            .style("fill", "none")
-    )
-);
-
-
-
-// from age of 0 to 100, go from green to red.
-
-const ageRangeColor = d3.scaleLinear<string, number>()
-    .range(["#ced3e5", "#164d72"])
-    .domain([0, 100]);
-
-// Creation of birthday rectangles
-svg.selectAll()
-    .data(birthdayData)
+svg.selectAll("g")
+    .data(Array.from(skyDataMap.entries()))
     .enter()
-    .append("rect")
-    .attr("x", d => xScale(String(d.month)) )
-    .attr("y", d => yScale(String(d.day)) )
-    .attr("width", xScale.bandwidth())
-    .attr("height", yScale.bandwidth())
-    .style("fill", d => ageRangeColor(d.age))
+    .append("g")
+    .selectAll("g")
+    .data(d => d[1])
+    .enter()
+    .append("g")
+    .attr("transform", (d, i) => `translate(${xScale(i + 1)}, ${yScale(d.minute)})`)
+    .each(function (d: SkyEntry) {
+        const group = d3.select(this);
+        const outerTriangle = group.append("polygon")
+            .attr("points", (_, i) => {
+                const halfSize = triangleSize / 2;
+                return `0, ${-halfSize} ${halfSize},${halfSize} ${-halfSize},${halfSize}`;
+            })
+            .attr("fill", "#5077c7")
+
+        const innerTriangleSize = triangleSize / innerTriangleScale(d.size); // Adjust the size of the inner triangle as needed
+
+        const innerTriangle = group.append("polygon")
+            .attr("points", (d, i) => {
+                const halfSize = innerTriangleSize / 2;
+                return `0, ${-halfSize} ${halfSize},${halfSize} ${-halfSize},${halfSize}`;
+            })
+            .attr("fill", (d: SkyEntry) => colorScale(d.cloudCov))
+
+            .on("mouseover", onMouseOver)
+            .on("mousemove", onMouseMove)
+            .on("mouseleave", onMouseLeave);
+    });
+
+
+// FIRST ITERATION
+
+/*
+
+svg.selectAll("g")
+    .data(Array.from(skyDataMap.entries()))
+    .enter()
+    .append("g")
+    .selectAll("polygon")
+    .data(d => d[1])
+    .enter()
+    .append("polygon")
+    .attr("points", (d, i) => {
+        const x = xScale(i + 1);
+        const y = yScale(d.minute);
+        const halfSize = triangleSize / 2;
+
+        return `${x - halfSize},${y} ${x + halfSize},${y} ${x},${y - (i % 2 === 0 ? triangleSize : -triangleSize)}`;
+    })
+    .attr("fill", d => colorScale(d.cloudCov))
+    .attr("stroke", "blue")
     .on("mouseover", onMouseOver)
     .on("mousemove", onMouseMove)
     .on("mouseleave", onMouseLeave);
+
+
+ */
